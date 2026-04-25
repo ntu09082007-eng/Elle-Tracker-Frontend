0fc805fabc459e6ccd56cc307d2a06cc76207142
ntu09082007-eng <ntu09082007@gmail.com>
2026-04-26 01:28:58 +0700

    Remove Header component from App

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import RealtimePage from "./pages/RealtimePage";
 import StatsPage from "./pages/StatsPage";
 import PredictionPage from "./pages/PredictionPage";
 import AboutPage from "./pages/AboutPage";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import EmptyPage from "./pages/EmptyPage";
 
@@ -16,7 +15,6 @@ export default function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white flex flex-col overflow-x-hidden">
-        <Header />
         <main className="flex-grow w-full mb-[20px] min-h-[calc(100vh-80px)]">
           <div className="w-full px-4 md:px-6 py-8">
             <Routes>
